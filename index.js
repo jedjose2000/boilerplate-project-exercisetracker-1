daf1acaf02bfcc7dda64b62492c807f51bf35e2f
jedjose2000 <jedjose2000@gmail.com>
2023-10-23 02:25:50 +0000

    finishing all requirements

--- index.js
+++ index.js
@@ -2,7 +2,28 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+mongoose = require('mongoose');
+let bodyParser = require('body-parser');
+const dns = require('node:dns');
+let uri = 'mongodb+srv://jedjose2000:hyPhvDwFltCj0Wkb@cluster0.cf7vwhh.mongodb.net/db_freeCodeCamp?retryWrites=true&w=majority'
 
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+// let UserSchema = new mongoose.Schema({
+//   user_id: { type: String, required: true, unique: true },
+//   username: { type: String, required: true, unique: true }
+// })
+
+
+// let ExerciseTrackerSchema = new mongoose.Schema({
+//   user_id: { type: String, required: true },
+//   description: { type: String, required: true }
+// })
+
+
+
+app.use("/", bodyParser.urlencoded({ extended: false }));
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
@@ -11,8 +32,15 @@ app.get('/', (req, res) => {
 
 
 
-
-
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
+
+
+app.post('/api/users', (req, res) => {
+  let username = req.body.username;
+  res.json({username: username})
+});
+
+
+
